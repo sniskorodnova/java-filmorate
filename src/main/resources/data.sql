ecb94575a8f498808b5c92dc7caddd1ac3edb3d5
oastolyarov <86529011+oastolyarov@users.noreply.github.com>
2022-06-07 20:30:30 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/* MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (1, 'G');
+MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (1, 'G');
 MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (2, 'PG');
 MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (3, 'PG-13');
 MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (4, 'R');
@@ -9,4 +9,4 @@ MERGE INTO genre (GENRE_ID, NAME) VALUES (2, 'Drama');
 MERGE INTO genre (GENRE_ID, NAME) VALUES (3, 'Cartoon');
 MERGE INTO genre (GENRE_ID, NAME) VALUES (4, 'Thriller');
 MERGE INTO genre (GENRE_ID, NAME) VALUES (5, 'Documentary');
-MERGE INTO genre (GENRE_ID, NAME) VALUES (6, 'Action'); */
+MERGE INTO genre (GENRE_ID, NAME) VALUES (6, 'Action');
