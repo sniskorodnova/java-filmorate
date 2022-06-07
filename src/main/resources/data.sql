1dfe1262e0533eb57009f63f16a907bb1eb2c45f
Oleg Stolyarov <oastolyarov@gmail.com>
2022-06-07 15:35:19 +0300

    - добавил дроп таблиц перед каждым запуском; - закомментил создание полей genres и rating_mpaa в data.sql, чтобы не создавались лишний раз; - обновил названия таблиц во всех запросах; - обновил pom.xml, добавил туда версию р2database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-MERGE INTO "rating_mpaa" (RATING_MPAA_ID, NAME) VALUES (1, 'G');
-MERGE INTO "rating_mpaa" (RATING_MPAA_ID, NAME) VALUES (2, 'PG');
-MERGE INTO "rating_mpaa" (RATING_MPAA_ID, NAME) VALUES (3, 'PG-13');
-MERGE INTO "rating_mpaa" (RATING_MPAA_ID, NAME) VALUES (4, 'R');
-MERGE INTO "rating_mpaa" (RATING_MPAA_ID, NAME) VALUES (5, 'NC-17');
+/* MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (1, 'G');
+MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (2, 'PG');
+MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (3, 'PG-13');
+MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (4, 'R');
+MERGE INTO rating_mpaa (RATING_MPAA_ID, NAME) VALUES (5, 'NC-17');
 
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (1, 'Comedy');
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (2, 'Drama');
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (3, 'Cartoon');
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (4, 'Thriller');
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (5, 'Documentary');
-MERGE INTO "genre" (GENRE_ID, NAME) VALUES (6, 'Action');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (1, 'Comedy');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (2, 'Drama');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (3, 'Cartoon');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (4, 'Thriller');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (5, 'Documentary');
+MERGE INTO genre (GENRE_ID, NAME) VALUES (6, 'Action'); */
