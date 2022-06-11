0f9d3d2bdd75e846ffb5b9de744bd93d035f52ba
semin <semin.aleksey1@yandex.ru>
2022-06-11 18:56:48 +0300

    test

---
+++ src/main/java/ru/yandex/practicum/filmorate/aspect/FilmServiceAspect.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import ru.yandex.practicum.filmorate.model.Film;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FilmServiceAspect {
+
+    @Pointcut("execution(* ru.yandex.practicum.filmorate.service.FilmService.likeFilm(..))")
+    public void likeFilm() {}
+
+    @After("likeFilm()")
+    public void afterLikeFilm(JoinPoint joinPoint) {
+        Object[] signatureArgs = joinPoint.getArgs();
+        Long filmId = (Long)signatureArgs[0];
+        Long userId = (Long)signatureArgs[1];
+        // Здесь мы будем писать в базу
+        System.out.println("userId " + userId + " поставил лайк фильму с filmId: " + filmId);
+    }
+
+}
