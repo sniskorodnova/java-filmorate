6180d1a7408dd77fa2540174a0b183adb7226da7
Sveta Niskorodnova <sveta.niskorodnova@gmail.com>
2022-06-15 22:33:14 +0300

    fix: поправить метод удаления отзыва в Aspect

--- src/main/java/ru/yandex/practicum/filmorate/aspect/EventFeedAspect.java
+++ src/main/java/ru/yandex/practicum/filmorate/aspect/EventFeedAspect.java
@@ -163,7 +163,7 @@ public class EventFeedAspect {
         //инициализация полей для объекта класса Feed
         long timestamp = getTimeNow();
         Long userId = (Long) joinPoint.getArgs()[0];
-        Long entityId = feedStorage.getReviewIdByUserId(userId, ((Review) joinPoint.getArgs()[0]).getFilmId());
+        Long entityId = feedStorage.getReviewIdByUserId(userId, (Long) joinPoint.getArgs()[0]);
         if (entityId != null) {
             //запись в хранилище
             feedStorage.createEvent(
